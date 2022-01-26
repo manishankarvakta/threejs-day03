91e5e999aeddf95eff07efb48e52dddc3f24528d
Manishankar Vakta <manishankarvakta@gmail.com>
2022-01-26 21:55:41 +0600

    Understanding the group

--- src/script.js
+++ src/script.js
@@ -10,10 +10,32 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
+group.position.y = 1
+group.rotation.y = Math.PI * 0.25
+group.scale.set(.5, .5, .5)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x= -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x= 2
+group.add(cube3)
+
 
 /**
  * Sizes
@@ -28,7 +50,13 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.position.y = 1
+// camera.position.x = 1
 scene.add(camera)
+
+// axis helper
+const axisHelper = new THREE.AxisHelper()
+scene.add(axisHelper)
 
 /**
  * Renderer
